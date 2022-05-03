22b7b0b2acf3137cbb436ca9d13c30fb8897a96d
Paul-Auguste <paulauguste.lacave@gmail.com>
2022-05-03 18:27:02 +0200

    [Feature] Added basic collision system to not go through enemy

--- Assets/Scripts/Gameplay/PlayerController.cs
+++ Assets/Scripts/Gameplay/PlayerController.cs
@@ -6,13 +6,21 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour, IJoystickController
 {
 	public  float m_Speed;
-
-	public Action OnMove;
-	public Action OnStop;
+	 
+	// CACHE
+	private Rigidbody m_RigidBody;
 	
-	// Buffer
+	public Action OnStartMove;
+	public Action OnStopMove;
+	
+	// BUFFER
 	private bool    m_IsMoving;
 	private Vector3 m_Direction;
+
+	private void Awake()
+	{
+		m_RigidBody = GetComponent<Rigidbody>();
+	}
 	
 	private void Start()
 	{
@@ -23,7 +31,7 @@ public class PlayerController : MonoBehaviour, IJoystickController
 	public void OnTouchDown()
 	{
 		m_Direction = Vector3.zero;
-		Move();
+		StartMove();
 	}
 
 	public void OnTouch(Vector2 direction)
@@ -33,29 +41,38 @@ public class PlayerController : MonoBehaviour, IJoystickController
 
 	public void OnTouchUp()
 	{
-		Stop();
+		StopMove();
 		m_Direction = Vector3.zero;
 	}
 
-	private void Move()
+	private void StartMove()
 	{
 		m_IsMoving = true;
-		OnMove?.Invoke();
+		OnStartMove?.Invoke();
 	}
 	
-	private void Stop()
+	private void StopMove()
 	{
 		m_IsMoving = false;
-		OnStop?.Invoke();
+		OnStopMove?.Invoke();
 	}
 
-	private void Update()
+	public void Move(Vector3 _Direction)
+	{
+		transform.forward = Vector3.Lerp(transform.forward, _Direction, Time.fixedDeltaTime * 12.0f);
+		m_RigidBody.MovePosition(m_RigidBody.position + _Direction.normalized * (m_Speed * Time.fixedDeltaTime));
+	}
+	
+	private void FixedUpdate()
 	{
 		if (!m_IsMoving)
 			return;
-			
-		transform.position += m_Direction.normalized * (m_Speed * Time.deltaTime);
+
+		Move(m_Direction.normalized);
 	}
 
-
+	public Vector3 GetPosition()
+	{
+		return m_RigidBody.position;
+	}
 }
