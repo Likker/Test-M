69d59a88110ae6a861be77b010955913106ab58f
Paul-Auguste <paulauguste.lacave@gmail.com>
2022-05-04 14:41:39 +0200

    [Feature] Tool tip for damages

--- Assets/Scripts/Gameplay/Character/Character.cs
+++ Assets/Scripts/Gameplay/Character/Character.cs
@@ -6,18 +6,22 @@ using UnityEngine;
 
 public abstract class Character : MappedObject, IHittable
 {
-	public  int m_Life;
-	private int m_MaxLife;
+	[SerializeField] private int        m_Life;
+	[SerializeField] private GameObject m_TooltipDamage;
 	
-	public  Action<float> OnHit { get; set; }
-	private BumpEffect    m_BumpEffect;
+	private int        m_MaxLife;
+	private BumpEffect m_BumpEffect;
 
+	public Action<float> OnHit { get; set; }
+	
 	protected override void Awake()
 	{
 		base.Awake();
 		m_BumpEffect = GetComponentInChildren<BumpEffect>();
 		m_MaxLife    = m_Life;
 	}
+	
+	
 	
 	public void TakeDamage(int _Damage)
 	{
@@ -26,10 +30,20 @@ public abstract class Character : MappedObject, IHittable
       
 		m_Life -= _Damage;
 		OnHit?.Invoke(Mathf.Clamp01((float)m_Life / (float)m_MaxLife));
-      
+		SpawnTooltipDamage(_Damage);
+		
 		if (m_Life <= 0)
 			Die();
 	}
-
+	
+	private void SpawnTooltipDamage(int _Damage)
+	{
+		var tooltipDamage =
+				PoolManager.Instance
+						   .SpawnObject(m_TooltipDamage, transform.position  + Vector3.up * 4.5f, Quaternion.identity)
+						   .GetComponent<TooltipDamageWorld>();
+		tooltipDamage.Play(_Damage, () => PoolManager.Instance.ReleaseObject(tooltipDamage));
+	}
+	
 	protected abstract void Die();
 }
